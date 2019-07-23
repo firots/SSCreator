254ce2d7cf94cbcd896f8184bfec1ba356c9f70c
Fırat <firat.ozkms@gmail.com>
2019-07-23 21:00:43 +0300

    * Program.cs: * CLParser.cs: * SSCreator.csproj: * SSCreator.dll: * SSCreator.pdb: * SSCreator.deps.json:
    * TemplateBase.cs: skiasharp

--- SSCreator/Program.cs
+++ SSCreator/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace SSCreator {
     class Program {
 
         static void Main(string[] args) {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
             CLParser.parse(args);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
         }
     }
 }
--- SSCreator/Templates/TemplateBase.cs
+++ SSCreator/Templates/TemplateBase.cs
@@ -5,13 +5,15 @@ using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.Primitives;
 using System.IO;
 using SixLabors.ImageSharp.Formats.Png;
+using SkiaSharp;
+
 namespace SSCreator {
     class TemplateBase {
 
         public virtual string name => "Base";
         public virtual int argCount => 3;
 
-        public virtual void generate(string[] args) { 
+        /*public virtual void generate(string[] args) { 
             string templatePath = getTemplatePath();
             using (Image<Rgba32> template = Image.Load(Path.Combine(templatePath, "base.jpg")))
             using (Image<Rgba32> screenShot = Image.Load(args[1]))
@@ -22,6 +24,30 @@ namespace SSCreator {
                     .DrawImage(screenShot, new Point(335, 339), 1f)
                 );
                 outputImage.Save(args[2]);
+            }
+        }*/
+
+        public virtual void generate(string[] args) {
+            SKBitmap template = new SKBitmap(1242, 2688);
+            using (SKCanvas canvas = new SKCanvas(template)) {
+                string templatePath = getTemplatePath();
+                var baseBuffer = File.ReadAllBytes(Path.Combine(templatePath, "base.jpg"));
+                SKBitmap baseBitMap = SKBitmap.Decode(baseBuffer);
+                canvas.DrawBitmap(baseBitMap, new SKPoint(0, 0), null);
+
+                var ssBuffer = File.ReadAllBytes(args[1]);
+                SKBitmap ssBitMap = SKBitmap.Decode(ssBuffer);
+                var info = new SKImageInfo(570, 994);
+                var ssResized = ssBitMap.Resize(info, SKFilterQuality.High);
+
+                canvas.DrawBitmap(baseBitMap, new SKPoint(0, 0), null);
+                canvas.DrawBitmap(ssResized, new SKPoint(335, 339), null);
+
+                using (FileStream fs = new FileStream(args[2], FileMode.Create, FileAccess.Write)) {
+                    SKData data = SKImage.FromBitmap(template).Encode(SKEncodedImageFormat.Png, 100);
+                    var bytes = data.ToArray();
+                    fs.Write(bytes, 0, bytes.Length);
+                }
             }
         }
 
