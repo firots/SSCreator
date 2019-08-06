f541767e44ee9a61c3c27d4771d2ddc46b1c9810
Fırat <firat.ozkms@gmail.com>
2019-08-07 00:54:38 +0300

    support for drawing rotated texts.

--- SSCreator/Generators/SSTextGenerator.cs
+++ SSCreator/Generators/SSTextGenerator.cs
@@ -23,9 +23,20 @@ namespace SSCreator {
                 paint.Typeface = getFont(text.fontName);
                 SKColor.TryParse(text.color, out SKColor color);
                 paint.Color = color;
-                var position = PositionHelper.getPosition(text.alignX, text.alignY, paint.MeasureText(text.text), text.fontSize, canvasSize);
+                var height = text.fontSize;
+                var width = paint.MeasureText(text.text);
+                var position = PositionHelper.getPosition(text.alignX, text.alignY, width, height, canvasSize);
                 position.y += text.fontSize;
-                canvas.DrawText(text.text, position.x, position.y, paint);
+                if (text.rotation.HasValue && text.rotation > 0) {
+                    using (SKPath path = new SKPath()) {
+                        path.MoveTo(width / 10, height - height / 10);
+                        path.LineTo(width - width / 10, height / 10);
+                        canvas.DrawTextOnPath(text.text, path, 0, 0, paint);
+                    }
+ 
+                } else {
+                    canvas.DrawText(text.text, position.x, position.y, paint);
+                }
             }
         }
 
